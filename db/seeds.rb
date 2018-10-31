16de86baf3156f0f76c746d46b92523d36574f90
Davis JR <davisjr2000@hotmail.com>
2018-10-31 14:47:30 -0300

    Added gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '55 44 66 53',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '17 42 35 54',
+    category:        'italian'
+  },
+  {
+    name:         'Vintao',
+    address:      'Wisard ,123-A',
+    phone_number:  '20 20 20 20',
+    category: 'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
